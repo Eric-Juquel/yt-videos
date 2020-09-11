565111111c035a46c323df232053292b5098c87a
eric J <e.juquel@woody-technologies.com>
2020-09-11 10:58:31 +0200

    ready to deploy

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,11 +4,7 @@ import VideoItem from "./VideoItem";
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map((video) => {
     return (
-      <VideoItem
-        key={video.id.videoId}
-        video={video}
-        onVideoSelect={onVideoSelect}
-      />
+      <VideoItem key={video.etag} video={video} onVideoSelect={onVideoSelect} />
     );
   });
 
